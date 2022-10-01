4e15b91c166285af39c79f179d80c79dc07ff97f
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2022-09-30 22:07:45 -0300

    feat: code commit into AULA_05

--- AULA_05/instance.tf
+++ AULA_05/instance.tf
@@ -7,14 +7,3 @@ resource "aws_instance" "this" {
     "Name" = format("%s-%s", var.machine_name, count.index)
   }
 }
-
-resource "aws_instance" "web" {
-  count         = var.amount
-  ami           = "ami-01b7be8d0c978c77c"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.this.id
-  tags = {
-    "Name" = "${var.machine_name}-instance-web-${count.index.0}"
-    "Name" = "instance-instance-web"
-  }
-}
