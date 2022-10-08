2c6f9be3cbce7475d7ade9a0a53b156be7ea8124
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2022-10-07 21:54:23 -0300

    demo: inicializando projeto final

--- AULA_08/demo_final_project/main.tf
+++ AULA_08/demo_final_project/main.tf
@@ -2,10 +2,13 @@
 
 module "aws_vpc" {
   source     = "../final_project/aws_vpc"
+  
   cidr_block = "10.0.0.0/16"
 }
 
 module "aws_subnet_private" {
+  source                  = "../final_project/aws_subnet"
+
   az_count                = var.az_count
   resource_vpc_id         = module.aws_vpc.id
   resource_vpc_cidr_block = module.aws_vpc.cidr_block
@@ -13,6 +16,8 @@ module "aws_subnet_private" {
 }
 
 module "aws_subnet_public" {
+  source                  = "../final_project/aws_subnet"
+
   az_count                = var.az_count
   resource_vpc_id         = module.aws_vpc.id
   resource_vpc_cidr_block = module.aws_vpc.cidr_block
